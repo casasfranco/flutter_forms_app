517688d4fd9e52efe23ad20f3f728139063b0e58
Franco Casas <franco.casas@directa24.com>
2023-05-30 17:18:01 -0300

    FormZ

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -43,42 +43,27 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
-
-  @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final password = registerCubit.state.password;
 
     return Form(
-      key: _formKey,
       child: Column(children: [
         CustomTextFormField(
           label: 'Nombre de usuario',
-          onChange: (value) {
-            registerCubit.usernameChanged(value);
-            _formKey.currentState?.validate();
-          },
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) return 'Debe tener al menos 6 letras';
-            return null;
-          },
+          onChange: registerCubit.usernameChanged,
+          errorMessage: username.errorMessage,
         ),
         const SizedBox(height: 10),
         CustomTextFormField(
           label: 'Correo electrónico',
           onChange: (value) {
             registerCubit.emailChanged(value);
-            _formKey.currentState?.validate();
           },
           validator: (value) {
             if (value == null || value.isEmpty) return 'Campo requerido';
@@ -94,23 +79,14 @@ class _RegisterFormState extends State<_RegisterForm> {
         const SizedBox(height: 10),
         CustomTextFormField(
           label: 'Contraseña',
-          onChange: (value) {
-            registerCubit.passwordChanged(value);
-            _formKey.currentState?.validate();
-          },
+          onChange: registerCubit.passwordChanged,
           obscureText: true,
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) return 'Debe tener al menos 6 letras';
-            return null;
-          },
+          errorMessage: password.errorMessage,
         ),
         const SizedBox(height: 20),
         FilledButton.tonalIcon(
           onPressed: () {
-            final isValid = _formKey.currentState!.validate();
-            if (isValid) registerCubit.onSubmit();
+            registerCubit.onSubmit();
           },
           icon: const Icon(Icons.save),
           label: const Text("Crear usuario"),
